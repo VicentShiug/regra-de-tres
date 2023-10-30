af5296ca08243310a4bde54dc8e0dc9fe9f3b595
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-10-30 12:17:41 -0300

    feat: credits and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import Input from './components/Form'
 
 function App () {
   return (
-    <div className="calculator-container">
-      <h1>Regra de três</h1>
-      <div className="input-container">
-        <Input />
+    <>
+
+      <div className="calculator-container">
+        <h1>Regra de três</h1>
+        <div className="input-container">
+          <Input />
+        </div>
       </div>
-    </div>
+      <footer>
+        <div className="social">
+          <a href="https://github.com/VicentShiug" target='_blank' rel="noreferrer">
+            Guilherme Câmara &copy; 2023
+          </a>
+        </div>
+        <div className='credits'>
+          <p>Créditos de layout</p>
+          <a href="https://www.youtube.com/@larissakich" target='_blank' rel="noreferrer">Larissakich</a>
+        </div>
+      </footer>
+    </>
   )
 }
 
